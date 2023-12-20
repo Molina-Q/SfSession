05b4d14bdd5b31e3e5f8cb31a682f70d87f5cc0a
Molina-Q <quentinmolina1@gmail.com>
2023-12-20 16:57:15 +0100

    createLabel

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Formation;
 use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\CreateFormationRequestFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +21,36 @@ class FormationController extends AbstractController
         return $this->render('formation/index.html.twig', [
             'controller_name' => 'FormationController',
             'formations' => $formations
+        ]);
+    }
+
+    #[Route('/formation/create', name: 'create_formation')]
+    public function create(
+        FormationRepository $formationRepository,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+
+        $form = $this->createForm(CreateFormationRequestFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $formation = new Formation;
+            $formation->setLabel($form->get('label')->getData());
+
+            $entityManager->persist($formation);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'The formation '.$formation->getLabel().' was successfully added');
+
+            return $this->redirectToRoute('create_formation');
+        }
+
+        $formations = $formationRepository->findBy([], ['label' => 'ASC']);
+
+        return $this->render('formation/create.html.twig', [
+            'createFormationForm' => $form->createView(),
         ]);
     }
 }
