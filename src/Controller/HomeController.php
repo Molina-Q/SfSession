a09b81a0012cb4817d87242d65333e9c978b7f3e
Molina-Q <quentinmolina1@gmail.com>
2023-12-20 16:15:36 +0100

    homePage update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FormationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
+
+        $formations = $formationRepository->findBy([], ['label' => 'ASC']);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'formations' => $formations
         ]);
     }
 }
